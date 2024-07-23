1794c303b07cb097d288445bc60b69cbd795769f
Kelvin Sidharta Sie <c14210307@john.petra.ac.id>
2024-07-24 01:07:01 +0700

    feat: navbar and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Button} from "primeng/button";
+import {fontAwesomeIcons} from "./shared/font-awesome-icons";
+import {FaIconComponent, FaIconLibrary} from "@fortawesome/angular-fontawesome";
+import {NavbarComponent} from "./layout/navbar/navbar.component";
+import {FooterComponent} from "./layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Button, FaIconComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'airbnb-frontend';
+export class AppComponent implements OnInit{
+  faIconLibrary = inject(FaIconLibrary)
+
+  ngOnInit(): void {
+    this.initFontAwesomeIcons()
+  }
+
+  private initFontAwesomeIcons() {
+    this.faIconLibrary.addIcons(
+      ...fontAwesomeIcons
+    )
+  }
 }
---
+++ src/app/layout/navbar/navbar.component.ts
@@ -0,0 +1,52 @@
+import {Component, OnInit} from '@angular/core';
+import {ButtonModule} from "primeng/button";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {ToolbarModule} from "primeng/toolbar";
+import {MenuModule} from "primeng/menu";
+import {CategoryComponent} from "./category/category.component";
+import {AvatarComponent} from "./avatar/avatar.component";
+import {DialogService} from "primeng/dynamicdialog";
+import {MenuItem} from "primeng/api";
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    ButtonModule,
+    FontAwesomeModule,
+    ToolbarModule,
+    MenuModule,
+    CategoryComponent,
+    AvatarComponent,
+  ],
+  providers: [DialogService],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent implements OnInit {
+  location = "Anywhere";
+  guest = "Add guests";
+  dates = "Add week";
+
+  // login () => this.authService.login();
+  //
+  // logout () => this.authService.logout();
+
+  currentMenuItems: MenuItem[] | undefined = [];
+
+  ngOnInit(): void {
+    this.fetchMenu();
+  }
+
+  private fetchMenu() {
+    return [
+      {
+        label: "Sign up",
+        styleClass: "font-bold",
+      },
+      {
+        label: "Log in"
+      }
+    ]
+  }
+}
